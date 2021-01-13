f7bf7f295dbe7e79f383fa53ec528dee3e39417d
João Luiz <jlc.reis@outlook.com>
2021-01-12 23:31:13 -0300

    primeiro template

--- sitelusartes/loja/urls.py
+++ sitelusartes/loja/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.LojaIndex, name='index'),
+    path('produtos/', views.LojaProdutos, name='produtos'),
 ]
--- sitelusartes/loja/views.py
+++ sitelusartes/loja/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def LojaIndex (request):
     return render (request, 'home.html')
+
+def LojaProdutos (request):
+    return render (request, 'produtos.html')
